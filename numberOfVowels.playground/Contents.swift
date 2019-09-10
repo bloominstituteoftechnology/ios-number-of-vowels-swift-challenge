41a1d2cc703a233718fa3ebd9949a8b9ca897085
Denis Cedeno <denis.cedeno@gmail.com>
2019-09-09 21:34:56 -0400

    number vowel Challenge submisson #1

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,25 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+    var numVowels = 0
     
+    for vowel in string{
+
+        switch vowel{
+        case "a", "e", "i", "o", "u", "A", "E", "I", "O", "U":
+            numVowels += 1
+
+        case "y", "Y":
+            if isYAVowel == true{
+                numVowels += 1
+            }
+        default:
+            continue
+        }
+        
+    }
+    return numVowels
 }
+
+print(numberOfVowels(in: "hello Kitty", isYAVowel: false))
+print(numberOfVowels(in: "hello Kitty", isYAVowel: true))
