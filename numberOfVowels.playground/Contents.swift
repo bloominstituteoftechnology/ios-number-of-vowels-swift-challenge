b9a2e4af48d0286631d4f39dda398942ae13ed58
Kai Sheng You <kaiyousheng@gmail.com>
2019-09-09 22:47:13 -0700

    Number Of Vowels

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,17 +1,25 @@
 import UIKit
 
-func numberOfVowels(str: String, isYAVowel: Bool = false) -> Int {
-    
-    let nums:int = 0;
-   
-    
-    for char in str {
-        
-        if char == ("a" || "e" || "i" || "o" || "u")
-        nums ++
+func numberOfVowels(in str:String, in isYAVowel : Bool = false) -> Int {
+
+    guard !str.isEmpty else {
+        print("It is an empty string")
+        return 0
+
+    }
+
+    let vowels: [Character] = isYAVowel ? ["a","e","i","o","u","y"] : ["a","e","i","o","u"]
+
+    var nums:Int = 0;
+    for character in str.lowercased() {
+        if vowels.contains(character){
+        nums += 1
         }
-        
-       return nums
+    }
+    return nums
 }
 
-print
+
+print(numberOfVowels(in: "aeiouys",in: true))
+
+print(numberOfVowels(in: "Hello World!"))        // returns 3
