aeed1e684870d2d2c3910a8892f51c4b647847cb
Crystal Rainn <CrystalRainn@icloud.com>
2019-09-09 21:27:51 -0400

    Completed Vowel Challenge

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,24 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+    var vowelCount: Int = 0
+    var consonantCount: Int = 0
+    for character in string {
+        switch character {
+        case "a", "e", "i", "o", "u":
+         vowelCount += 1
+        case "y":
+            if isYAVowel == true {
+                vowelCount += 1
+            } else {
+                consonantCount += 1
+            }
+        default:
+            consonantCount += 1
+        }
+    }
+    return vowelCount
 }
+
+numberOfVowels(in: "The quick brown fox jumped over the lazy dog.")
+numberOfVowels(in: "The quick brown fox jumped over the lazy dog.", isYAVowel: true)
