8efe73b1b0a86e6f4e59c4f015438fa7d1120a14
Fabiola <saga.fabiola@gmail.com>
2019-09-10 21:11:19 -0500

    It's counting vowels at least

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -2,18 +2,18 @@ import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     var vowel = 0
-    var characters: [Character] = []
+    let vowels: [Character] = ["a", "e", "i", "o", "u"]
     for character in string {
-        characters.append(character)
-        let vowels = "a"
-        if characters.contains(Character(vowels)) {
+        
+        if vowels.contains(character) {
+            vowel += 1
+        } else if isYAVowel == true {
             vowel += 1
         }
     }
-
     return vowel
-    
 }
 
 numberOfVowels(in: "Fabiola", isYAVowel: false)
+
 
