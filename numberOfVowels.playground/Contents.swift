d15ac41815069f04ae4a61091a2479325e85c1af
Fabiola <saga.fabiola@gmail.com>
2019-09-10 20:59:27 -0500

    🤷

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,19 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+    var vowel = 0
+    var characters: [Character] = []
+    for character in string {
+        characters.append(character)
+        let vowels = "a"
+        if characters.contains(Character(vowels)) {
+            vowel += 1
+        }
+    }
+
+    return vowel
     
 }
+
+numberOfVowels(in: "Fabiola", isYAVowel: false)
+
