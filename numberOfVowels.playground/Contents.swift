10f9736d76f906a1083ada23865dc984c7bda9c0
Jessie Griffin <i.jessie.me@icloud.com>
2019-09-10 21:36:51 -0400

    Working function with switch statement in a for...in loop

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,38 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+    var numOfVowels: Int = 0
     
+    for letter in string {
+        switch letter {
+        case "a",
+             "A":
+            numOfVowels += 1
+        case "e",
+             "E":
+            numOfVowels += 1
+        case "i",
+             "I":
+            numOfVowels += 1
+        case "o",
+             "O":
+            numOfVowels += 1
+        case "u",
+             "U":
+            numOfVowels += 1
+        case "y",
+             "Y":
+            if isYAVowel {
+                numOfVowels += 1
+            } else {
+                break
+            }
+        default:
+            break
+        }
+    }
+    
+    return numOfVowels
 }
+
+print(numberOfVowels(in: "This is my fight song", isYAVowel: true))
