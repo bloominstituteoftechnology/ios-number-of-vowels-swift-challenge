b8e775a90d4e036ff5219c9b79042a523a88395f
Dennis <dpr1996@gmail.com>
2019-09-11 11:17:38 -0400

    Challenge Complete

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -2,4 +2,34 @@ import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     
+    var vowelCount: Int = 0
+    
+    if isYAVowel == true {
+        for character in string {
+            switch character {
+            case "a", "e", "i", "o", "u", "y":
+                vowelCount += 1
+            default:
+                vowelCount += 0
+            }
+        }
+    } else {
+        for character in string {
+            switch character {
+            case "a", "e", "i", "o", "u":
+                vowelCount += 1
+            default:
+                vowelCount += 0
+            }
+            
+            
+        }
+    }
+return vowelCount
+    
 }
+
+
+numberOfVowels(in: "Polly wants a cracker", isYAVowel: true)
+numberOfVowels(in: "Polly doesnt want a cracker", isYAVowel: true)
+numberOfVowels(in: "Yellow is a color", isYAVowel: false)
