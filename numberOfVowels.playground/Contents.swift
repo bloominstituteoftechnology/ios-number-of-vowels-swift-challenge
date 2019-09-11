d5fc8b9410521729258c8d6d00640cff39a669e2
Joel Groomer <joelgroomer@gmail.com>
2019-09-10 19:00:25 -0700

    Done.

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,31 @@
 import UIKit
 
-func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+func numberOfVowels(in testString: String, isYAVowel: Bool = false) -> Int {
+    var vowels = 0
     
+    for c in testString {
+        switch c {
+        case "a", "e", "i", "o", "u":
+            vowels += 1
+        case "y":
+            if isYAVowel {
+                vowels += 1
+            }
+        default:
+            //do nothing
+            continue
+        }
+    }
+    return vowels
 }
+
+let string1 = "The quick brown fox jumped over the lazy dog."
+let string2 = "Working with strings is super fun!"
+let string3 = "Polly wants a cracker!"
+
+print(numberOfVowels(in: string1))
+print(numberOfVowels(in: string1, isYAVowel: true))
+print(numberOfVowels(in: string2))
+print(numberOfVowels(in: string2, isYAVowel: true))
+print(numberOfVowels(in: string3))
+print(numberOfVowels(in: string3, isYAVowel: true))
