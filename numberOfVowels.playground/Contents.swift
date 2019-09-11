03c3d3240ad941d2c6ace3d7323139cad926b95c
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-10 21:47:03 -0400

    finish the challenge

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,60 @@
 import UIKit
+//func numberOfVowels( in string: String, isYAVowel: Bool) -> Int {
+//    var number = 0
+//
+//    for i in string {
+//        if i == "Y" {
+//            if isYAVowel == true {
+//                number += 1
+//            }
+//        } else if i == "a" || i == "i" || i == "u" || i == "e" || i == "o" {
+//            number += 1
+//        }
+//    }
+//
+//    return number
+//
+//}
+//
+//func numberOfVowels( in string: String, isYAVowel: Bool) -> Int {
+//    var number = 0
+//    let lowercasedString = string.lowercased()
+//
+//    for letter in lowercasedString {
+//        if (letter == "y" && isYAVowel == true) ||
+//            letter == "a" ||
+//            letter == "i" ||
+//            letter == "u" ||
+//            letter == "e" ||
+//            letter == "o"
+//        {
+//            number += 1
+//        }
+//    }
+//    return number
+//}
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+    var number = 0
+    let lowercasedString = string.lowercased()
+    let vowels = ["a", "e", "i", "o", "u"]
     
+    for letter in lowercasedString {
+        if (letter == "y" && isYAVowel == true) || vowels.contains(String(letter)) {
+            number += 1
+        }
+    }
+    return number
 }
+
+// case without string and Y isn't included should be 0
+print(numberOfVowels(in: "", isYAVowel: false))
+
+// case with and Y is included should be 6
+print(numberOfVowels(in: "Polly wants a cracker!", isYAVowel: true))
+
+// case without vowels and Y isn't included should be 0
+print(numberOfVowels(in: "0908923"))
+
+// case with and Y isn't included should be 5
+print(numberOfVowels(in: "Polly wants a cracker!", isYAVowel: false))
