1f40fda14c627a19abb2a323725233c9641f220a
dillonp23 <dillonp.lambda@gmail.com>
2019-09-10 20:53:03 -0700

    simplify code

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -5,31 +5,19 @@ func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     var totalVowels = 0
     
     for vowel in stringLowercase {
-        
-        if isYAVowel == true {
-            var vowelCount = 0
-            switch vowel {
-            case "a", "e", "i", "o", "u", "y":
-                vowelCount += 1
-            default:
-                vowelCount = 0
-            }
-            totalVowels += vowelCount
-        } else {
-            var vowelCount = 0
             switch vowel {
             case "a", "e", "i", "o", "u":
-                vowelCount += 1
+                totalVowels += 1
+            case "y":
+                if isYAVowel == true {
+                    totalVowels += 1
+                }
             default:
-                vowelCount = 0
-            }
-            totalVowels += vowelCount
+                break
         }
-        
     }
-    
     return totalVowels
 }
 
 
-numberOfVowels(in: "polly want a cracker", isYAVowel: false)
+numberOfVowels(in: "polly want a crackeery!", isYAVowel: true)
