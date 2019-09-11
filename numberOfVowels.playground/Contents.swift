6bfefd5f22d4240f7672ec9a2288446d608736b1
Steven Farrell <stevenfarrelltraining@yahoo.com>
2019-09-10 21:57:48 -0400

    got it to print out each vowel, working to get it to print the count instead

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,24 @@
 import UIKit
 
+let myVowels: [String] = ["a", "e", "i", "o", "u"]
+
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+   for vowel in string {
+    if vowel == "a" {
+        print("a")
+    } else if vowel == "e" {
+        print("e")
+    } else if vowel == "i" {
+        print("i")
+    } else if vowel == "o" {
+        print("o")
+    } else if vowel == "u" {
+        print("u")
+    } else {
+        continue
+    }
 }
+    return myVowels.count
+}
+
+numberOfVowels(in: "Does polly want a Cracker or a Carrot?", isYAVowel: true)
