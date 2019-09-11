6c51374c8df0d3de120cfd5f73477141bdf1bfc5
Sam Gutierrez <samgutierrez@Sams-MacBook-Pro.local>
2019-09-10 21:02:38 -0600

    Ran out of time

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,12 @@
-import UIKit
+
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+    var count = 0
+    for vowels in numberOfVowels(in: "Sam likes to workout everyday") {
+        if vowels == "a", "e", "i", "o", "u", "y" {
+            count += 1
+            
+            return count
+        }
+    }
 }
