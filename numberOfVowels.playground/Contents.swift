824935953af8b1c5d6b4f32fa43fd0679f111d86
Jessie Griffin <i.jessie.me@icloud.com>
2019-09-10 21:56:01 -0400

    converted string to all lowercase to  minimize code is switch cases

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -2,12 +2,13 @@ import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     var numOfVowels: Int = 0
+    let string = string.lowercased()
     
     for letter in string {
         switch letter {
-        case "a", "A", "e", "E", "i", "I", "o", "O", "u", "U":
+        case "a", "e", "i", "o", "u":
             numOfVowels += 1
-        case "y", "Y":
+        case "y":
             if isYAVowel {
                 numOfVowels += 1
             } else {
@@ -24,3 +25,4 @@ func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
 print(numberOfVowels(in: "This is my fight song", isYAVowel: true))
 print(numberOfVowels(in: "There are 11 vowels in this sentence."))
 print(numberOfVowels(in: "Without the letter y, you have 17 vowels in this sentence.")) // isYAVowel has an initial value of false
+print(numberOfVowels(in: "If I use Uppercase letters, I should get 16 vowels."))
