32de9858513e2c6c70fae57895dab5687b05a459
dillonp23 <dillonp.lambda@gmail.com>
2019-09-10 18:30:06 -0700

    Complete challenge submission

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,35 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+    let stringLowercase = string.lowercased()
+    var totalVowels = 0
     
+    for vowel in stringLowercase {
+        
+        if isYAVowel == true {
+            var vowelCount = 0
+            switch vowel {
+            case "a", "e", "i", "o", "u", "y":
+                vowelCount += 1
+            default:
+                vowelCount = 0
+            }
+            totalVowels += vowelCount
+        } else {
+            var vowelCount = 0
+            switch vowel {
+            case "a", "e", "i", "o", "u":
+                vowelCount += 1
+            default:
+                vowelCount = 0
+            }
+            totalVowels += vowelCount
+        }
+        
+    }
+    
+    return totalVowels
 }
+
+
+numberOfVowels(in: "polly want a cracker", isYAVowel: false)
