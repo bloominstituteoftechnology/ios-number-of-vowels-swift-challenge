67a4aad9c6f47207cb4d26e4f4af6515dfe26a14
Robert Keffury <bobby.keffury@me.com>
2019-09-10 19:20:02 -0700

    Finished the count for the vowels.

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,13 +1,24 @@
 import UIKit
 
-var vowels = ["a","e","i","o","u"]
+
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     
+    let vowels = ["a","e","i","o","u","A","E","I","O","U"]
+    var count: Int = 0
+    
+    for character in string {
+        if vowels.contains("\(character)") {
+            count += 1
+        }
+    }
     
     
-   
+    
+   return count
 }
+
+
 
 
 
