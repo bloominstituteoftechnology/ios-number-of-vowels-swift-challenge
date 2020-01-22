b1ceeb746c86f6d2bf4fd0dded1191c5130a16ff
Keri Levesque <kerianne116@gmail.com>
2020-01-22 11:14:35 -0500

    psuedo code the problem first

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,22 @@
 import UIKit
 
+ /*"A, E, I, O, U, and sometimes Y..."
+
+Write a function called numberOfVowels(in string: String) that returns the count of the total number of vowels in a string. Your solution should be case-insensitive, and allow for 'Y' to be included, or excluded from the count when calling the function.
+
+Example: numberOfVowels(in: "Polly wants a cracker!", isYAVowel = true) // returns 6 */
+
+
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     
+    for vowel in {
+        
+    }
+    
 }
+
+// need to loop through given string
+//check if characters are vowel
+//count each vowel
+//print the number
+
