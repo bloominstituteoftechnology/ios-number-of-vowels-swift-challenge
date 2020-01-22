faf90f1aba8a93ca7084ecb79e2e2b1968042b70
Samuel Esserman <samessermanlambda@gmail.com>
2020-01-22 11:36:35 -0500

    Third commit, after restarting

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -3,10 +3,7 @@ import UIKit
 let vowels: [Character] = ["a", "e","i","o","u","y"]
 
 func countVowels(in string: String) -> Int {
-    var numberOfVowels: Int = 0
-    for vowel in vowels {
-        print(vowels.count)
-    }
-    return countVowels(in: "Write a function called numberOfVowels(in string: String) that returns the count of the total number of vowels in a string. Your solution should be case-insensitive, and allow for 'Y' to be included, or excluded from the count when calling the function.")
-}
+    return vowels.count
 
+}
+print (countVowels(in: "Write a function called numberOfVowels(in string: String) that returns the count of the total number of vowels in a string. Your solution should be case-insensitive, and allow for 'Y' to be included, or excluded from the count when calling the function."))
