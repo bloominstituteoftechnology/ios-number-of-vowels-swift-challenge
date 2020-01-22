0bd9ea78a956cffc118212fff6fd4b760d7c988f
Nichole Davidson <nicholedavid@gmail.com>
2020-01-22 09:35:15 -0700

    Finished

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -2,4 +2,20 @@ import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     
+    var vowelCount = 0
+    
+    for character in string {
+        
+        switch character {
+        case "a", "e", "i", "o", "u", "y":
+            vowelCount += 1
+        default:
+            break
+        }
+    }
+    return vowelCount
 }
+
+var vowelsInString = numberOfVowels(in: "Quack, quack, quack, Mr. Ducksworth", isYAVowel: true)
+
+print(vowelsInString)
