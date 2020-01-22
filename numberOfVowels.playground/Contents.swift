0f1f4f7e020c9ac68482b54f782000a84b46ca05
Enrique Gongora <EGTech24@gmail.com>
2020-01-22 08:41:53 -0800

    Completed Vowel Code Challenge

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,32 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+    var totalVowels: Int = 0
     
+    for vowels in string{
+        switch vowels{
+        case "a", "e", "i", "o", "u", "A", "E", "I", "O", "U":
+            totalVowels += 1
+        case "y", "Y":
+            if isYAVowel{
+            totalVowels += 1
+            }
+        default:
+            break
+        }
+    }
+    return totalVowels
 }
+
+//MARK: - Examples
+
+
+//Should print 10
+let test = numberOfVowels(in: "a e i o u A E I O U ", isYAVowel: false)
+print(test)
+
+
+//Should print 6
+let gitHubExample = numberOfVowels(in: "Polly wants a cracker!", isYAVowel: true)
+print(gitHubExample)
+
