ac07dc73a27a5128a768ced076bedd1899191920
Samuel Esserman <samessermanlambda@gmail.com>
2020-01-22 11:42:25 -0500

    Sixth commit, close but still not working

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 let vowels: [Character] = ["a", "e","i","o","u","y"]
 
 func countVowels(in string: String) -> Int {
-    return vowels.count
+    return countVowels(in: "Sam")
 
 }
 print (countVowels(in: "A, E, I, O, U, Y"))
