c62f3888930aeff450b4b6148a1feb9bfc2cae5a
Samuel Esserman <samessermanlambda@gmail.com>
2020-01-22 11:31:33 -0500

    Second Commit

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -2,6 +2,11 @@ import UIKit
 
 let vowels: [Character] = ["a", "e","i","o","u","y"]
 
-func numberOfVowels(in string: String, isYAVowel: Bool = true) -> Int {
-    var numberOfVowels: Int = 0 
+func countVowels(in string: String) -> Int {
+    var numberOfVowels: Int = 0
+    for vowel in vowels {
+        print(vowels.count)
+    }
+    return countVowels(in: "Write a function called numberOfVowels(in string: String) that returns the count of the total number of vowels in a string. Your solution should be case-insensitive, and allow for 'Y' to be included, or excluded from the count when calling the function.")
 }
+
