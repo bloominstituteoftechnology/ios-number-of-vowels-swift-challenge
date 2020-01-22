17c79085eb3ef6106b8365671824e3460a3f582f
Rebecca <mythreldrows@aol.com>
2020-01-22 11:54:24 -0500

    I didn't get it working.

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,31 @@
 import UIKit
 
-func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+func numberOfVowels(in sentence: String, isYAVowel: Bool = false) -> Int {
+    var vowelCount = 0 //this is where my count is stored.
     
+    if isYAVowel == false { //if isYAVowel is false, then it should run the code under here.
+        switch sentence {
+    case "a", "e", "i", "o", "u": // If there's any vowels, it should count them...
+        vowelCount += 1
+        print(vowelCount)
+        return vowelCount
+        default: //if there aren't any vowels, it should return having not counted anything.
+        return vowelCount
+        }
+    
+        } else {
+        
+        if isYAVowel == true { //if isYAVowel isn't false, it should come here and run this code if it's true.
+            switch sentence {
+            case "a", "e", "i", "o", "u", "y": //if there are vowels, y included, it should count them.
+             vowelCount += 1
+             print(vowelCount)
+             return vowelCount
+            default: //if there are no vowels, including y, it should not count them and move on.
+            return vowelCount
+            }
+        }
+    }
 }
+
+numberOfVowels(in: "I'm not good at coding!")
