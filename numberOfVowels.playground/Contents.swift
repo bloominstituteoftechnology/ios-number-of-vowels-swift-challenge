7374a1ad4fb9e49660a0e1a05aef255e5ff68bb6
Jordan Skenandore <jordan.skenandore@gmail.com>
2020-01-27 20:41:03 -0700

    Function created to determine if Y is a vowel. The function will print out the number of vowels as well as whether isYAVowel is true or not.

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,7 +1,32 @@
 import UIKit
 
-var vowelCount = 0
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+    var vowelCount = 0
+    if isYAVowel == false {
+        for letter in string {
+            switch letter {
+            case "a", "e", "i", "o", "u":
+                vowelCount += 1
+            default:
+                ()
+            }
+        }
+        print("Since \"Y\" is not a vowel, there are \(vowelCount) vowels in \"\(string)\"")
+    } else {
+        for letter in string {
+            switch letter {
+            case "a", "e", "i", "o", "u", "y":
+                vowelCount += 1
+            default:
+                ()
+            }
+        }
+        print("Since \"Y\" is a vowel, there are \(vowelCount) vowels in \"\(string)\"")
+    }
+    return vowelCount
 }
+
+numberOfVowels(in: "The quick brown fox jumped over the lazy dog", isYAVowel: false)
+
+numberOfVowels(in: "The quick brown fox jumped over the lazy dog", isYAVowel: true)
