d889a1f5697781713c949da94831fdadd0a78d58
Michael Kagan <mkagan532@gmail.com>
2020-01-27 23:44:16 -0500

    attempt 1

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,24 @@
 import UIKit
-
+let vowels = ["a", "e", "i", "o", "u"]
+var vowelsInString = 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+    switch vowels {
+    case ["a"]:
+        vowelsInString += 1
+    case ["e"]:
+        vowelsInString += 1
+    case ["i"]:
+        vowelsInString += 1
+    case ["o"]:
+        vowelsInString += 1
+    case ["u"]:
+        vowelsInString += 1
+        
+    default:
+        vowelsInString += 0
+        
+    }
+    return vowelsInString
 }
+
+numberOfVowels(in: "Hello", isYAVowel: false)
