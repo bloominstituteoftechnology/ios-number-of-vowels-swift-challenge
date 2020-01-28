2c780a2b3ef7130475231cc750cf096810818c01
Matt Martindale <martindalematt18@yahoo.com>
2020-01-27 20:34:44 -0700

    wrote function to calculate number of vowels in a string

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,36 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+//    let vowels: [String] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    var totalNumberOfVowels = 0
     
+    if !isYAVowel {
+        for letter in string {
+            switch letter {
+                case "a", "e", "i", "o", "u", "A", "E", "I", "O", "U":
+                    totalNumberOfVowels += 1
+                default:
+                    continue
+            }
+        }
+        print(totalNumberOfVowels)
+        return totalNumberOfVowels
+    } else {
+        for letter in string {
+            switch letter {
+            case "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y":
+                totalNumberOfVowels += 1
+            default:
+                continue
+            }
+        }
+        print(totalNumberOfVowels)
+        return totalNumberOfVowels
+    }
+
 }
+
+numberOfVowels(in: "happy", isYAVowel: true)
+numberOfVowels(in: "happy", isYAVowel: false)
+numberOfVowels(in: "supercalifragilisticexpialidocious", isYAVowel: true)
+numberOfVowels(in: "SUPERCALIFRAGILISTICEXPIALIDOCIOUS", isYAVowel: true)
