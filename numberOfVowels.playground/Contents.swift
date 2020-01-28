a9f703cae4f681c955eb4a204c53943a1feb40e0
Jarren Campos <jarrenmcampos@gmail.com>
2020-01-27 20:32:33 -0800

    Finished vowel checker

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,50 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
-}
+    //    var vowels = ["a","e","i","o","u"]
+    var counterVowels = [String]()
+    if isYAVowel == true {
+        //        vowels.append("y")
+        
+        for letters in string {
+            switch String(letters) {
+            case "a":
+                counterVowels.append(String(letters))
+            case "e":
+                counterVowels.append(String(letters))
+            case "i":
+                counterVowels.append(String(letters))
+            case "o":
+                counterVowels.append(String(letters))
+            case "u":
+                counterVowels.append(String(letters))
+            case "y":
+                counterVowels.append(String(letters))
+            default:
+                continue
+            }
+            }
+            }
+    else if (isYAVowel == false) {
+        for letters in string {
+            switch String(letters) {
+            case "a":
+                counterVowels.append(String(letters))
+            case "e":
+                counterVowels.append(String(letters))
+            case "i":
+                counterVowels.append(String(letters))
+            case "o":
+                counterVowels.append(String(letters))
+            case "u":
+                counterVowels.append(String(letters))
+            default:
+                continue
+            }
+            }
+    }
+            print(counterVowels.count)
+            return counterVowels.count
+        }
+        numberOfVowels(in: "eiuyaoasdqe", isYAVowel: true)
+
