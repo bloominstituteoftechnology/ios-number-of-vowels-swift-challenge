3800eff8b7a013991e65628b9ab284e275eb09d8
kendra rayne <raynekendra@gmail.com>
2020-01-27 23:56:39 -0500

    eh...?
    Can't figure out the isYAVowel thing... sorry for the type in Vowl lol.

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,22 @@
 import UIKit
 
-func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+func numberOfVowels(in string: String/*,isYAVowl: Bool)*/) -> Int {
+    //allow for y to be included or excluded from the count when calling the function. how the hecky.
+    let vowels = "aeiouyAEIOUY"
+        let consonants = "bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ"
+        var vowelCount = 0
+        var consonantCount = 0
+
+        for letter in string {
+            if consonants.contains(letter) {
+                consonantCount += 1
+            } else {
+                if vowels.contains(letter) {
+                    vowelCount += 1
+                }
+            }
+        }
+        return vowelCount
+    }
     
-}
+print(numberOfVowels(in: "I want a burrito"))
