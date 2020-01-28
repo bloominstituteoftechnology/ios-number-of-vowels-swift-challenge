1e10e7928aa9200f794a97bfaeac3d90d357657a
ldmayfield <ldonnamayfield@gmail.com>
2020-01-27 20:38:48 -0700

    Code Challenge playground

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,20 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+    // my notes: will need switch on letter, why am I using isYAVowel
     
+    var vowel = 0
+    
+    for letter in string {
+        switch String(letter) {
+        case "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y":
+            vowel += 1
+        default:
+            break
+        }
+        
+    }
+    return vowel
 }
+
+numberOfVowels(in: "AeschYlus")
