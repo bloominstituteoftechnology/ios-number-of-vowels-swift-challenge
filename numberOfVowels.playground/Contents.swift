a1fdfb8fd00fb9e8839c54f0cf175c75b3a9c837
Matt Martindale <martindalematt18@yahoo.com>
2020-02-13 19:17:29 -0700

    github acting weird

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,35 +1,52 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool) -> Int {
-    var totalNumberOfVowels = 0
     
-    if !isYAVowel {
-        for letter in string {
-            switch letter {
-                case "a", "e", "i", "o", "u", "A", "E", "I", "O", "U":
-                    totalNumberOfVowels += 1
-                default:
-                    continue
-            }
+    //the solution with much better code
+    var totalNumberOfVowels = 0
+    let vowels: [Character] = isYAVowel ? ["a", "e", "i", "o", "u", "y"] : ["a", "e", "i", "o", "u"]
+    
+    for character in string.lowercased() {
+        if vowels.contains(character) {
+            totalNumberOfVowels += 1
         }
-        print(totalNumberOfVowels)
-        return totalNumberOfVowels
-    } else {
-        for letter in string {
-            switch letter {
-            case "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y":
-                totalNumberOfVowels += 1
-            default:
-                continue
-            }
-        }
-        print(totalNumberOfVowels)
-        return totalNumberOfVowels
     }
-}
+    print(totalNumberOfVowels)
+    return totalNumberOfVowels
 
+//    if !isYAVowel {
+//        for letter in string {
+//            switch letter {
+//                case "a", "e", "i", "o", "u", "A", "E", "I", "O", "U":
+//                    totalNumberOfVowels += 1
+//                default:
+//                    continue
+//            }
+//        }
+//        print(totalNumberOfVowels)
+//        return totalNumberOfVowels
+//    } else {
+//        for letter in string {
+//            switch letter {
+//            case "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y":
+//                totalNumberOfVowels += 1
+//            default:
+//                continue
+//            }
+//        }
+//        print(totalNumberOfVowels)
+//        return totalNumberOfVowels
+//    }
+}
 numberOfVowels(in: "happy", isYAVowel: true)
 numberOfVowels(in: "happy", isYAVowel: false)
 numberOfVowels(in: "supercalifragilisticexpialidocious", isYAVowel: true)
 numberOfVowels(in: "SUPERCALIFRAGILISTICEXPIALIDOCIOUS", isYAVowel: true)
 
+//super fancy solution
+
+//func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+//    guard !string.isEmpty else { return 0 }
+//    let vowels: [Character] = isYAVowel ? ["a", "e", "i", "o", "u", "y"] : ["a", "e", "i", "o", "u"]
+//    return string.lowercased().filter() { vowels.contains($0) }.count
+//}
