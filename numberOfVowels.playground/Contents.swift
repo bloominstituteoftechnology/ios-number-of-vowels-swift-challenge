e45ac52db2e5165c8479452cef7ae777d016bd9d
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-05-06 08:41:05 -0700

    Coding Challenge

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,26 @@
 import UIKit
 
+var totalVowels = 0
+
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     
+    for vowels in string {
+        switch vowels {
+        case "a", "e", "i", "o", "u":
+            totalVowels += 1
+        case "y":
+            if isYAVowel == false {
+            totalVowels += 0
+        } else {
+            totalVowels += 1
+        }
+        default:
+            totalVowels += 0
+        }
+    }
+      return totalVowels
 }
+numberOfVowels(in: "Hello, good morning to the world", isYAVowel: true)
+
+
+print("total vowels is \(totalVowels)")
