40d95d67e1f1727b110fd52ae461348f99525a40
Vincent Hoang <vietnhoang7@gmail.com>
2020-05-06 08:16:20 -0700

    Complete challenge

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,23 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
+    let vowels = [ "a", "e", "i", "o", "u" ]
     
+    let vowelsY = [ "a", "e", "i", "o", "u", "y" ]
+    
+    
+    if isYAVowel {
+        return string.lowercased().filter({ vowelsY.contains(String($0)) }).count
+    }
+    
+    return string.lowercased().filter({ vowels.contains(String($0)) }).count
 }
+
+print(numberOfVowels(in: "aabbcc"))
+print(numberOfVowels(in: "AaBbCc"))
+
+print(numberOfVowels(in: "aEiOuy", isYAVowel: true))
+print(numberOfVowels(in: "azxzxEwrwritptpOjkjkughghyrtrt", isYAVowel: true))
+
+
+
