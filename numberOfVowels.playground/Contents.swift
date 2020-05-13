b9017ea20953df5c1f372076e7a72c9c9e0e50c1
Bryan Cress <8675309+cressbj@users.noreply.github.com>
2020-05-13 11:59:35 -0400

    incomplete solution, still working..

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -3,5 +3,19 @@
 import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+
+    let vowelString = "How many checks could a woodchuck chuck?"
+    for vowels in vowelString {
+
+        switch vowelString {
+        case "a", "e", "i", "o", "u":
+            print(vowelString)
+        default:
+            continue
+        }
+    }
+    return numberOfVowels(in: vowelString)
 }
+
+
+
