d4aa64def722e66e16063710a2b2fbc39a09caea
Mike Nichols <mike@nixco.net>
2020-05-13 10:33:41 -0500

    I think I got it but Xcode is running funny.  Will restart after commit.

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -2,4 +2,22 @@ import UIKit
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     
+    var vowels: [Character] = []
+    
+    for char in string.lowercased() {
+        switch char {
+        case "a", "e", "i", "o", "u":
+            vowels.append(char)
+        case "y":
+            if isYAVowel == true {
+                vowels.append(char)
+            }
+        default:
+            break
+        }
+    }
+    
+    return vowels.count
 }
+
+print(numberOfVowels(in: "Thank goodness it's Wednesday.", isYAVowel: true))
